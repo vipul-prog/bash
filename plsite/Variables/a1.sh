59aa5fbba7e77d0b4545a6bde2342619466827a6
vipul-prog <vipulanand@live.in>
2022-01-17 19:13:58 +0200

    Finished Variables

--- plsite/Variables/a1.sh
+++ plsite/Variables/a1.sh
@@ -1,15 +1,15 @@
 #!/bin/env bash
 
 #Directory
-directory=report
+directory="$2"
 
 #create the directory
-mkdir -p $directory
+mkdir -p -- "$directory"
 
 #User File
 file=shipments.csv
 
-argument=$1
+argument="$1"
 
 #Write Command
-grep $argument $file > $directory/$argument.csv
+grep -- "$argument" "$file" > "$directory/$argument.csv"
